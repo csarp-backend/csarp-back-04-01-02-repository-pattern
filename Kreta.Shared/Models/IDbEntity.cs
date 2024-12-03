e8542bd2568d02e83080d955b7c0b4d0c0561579
gycsaba <gycsaba@SZT9-24>
2024-12-03 15:54:49 +0100

    IDbEntity interface

---
+++ Kreta.Shared/Models/IDbEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Kreta.Shared.Models
+{
+    public interface IDbEntity<TEntity> where TEntity : class, new()
+    {
+        public Guid Id { get; set; }
+        public bool HasId => Id != Guid.Empty ? true : false;
+        public string GetDbSetName()
+        {
+            string str =new TEntity().GetType().Name;
+            return str.EndsWith("s") ? str.Substring(0, str.Length - 1) : str;
+        }
+    }
+}
