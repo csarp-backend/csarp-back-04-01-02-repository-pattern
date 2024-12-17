08b55ae450efab104b2aa90ffdce6dcc97ca52cc
gycsaba <gycsaba@SZT9-24>
2024-12-17 09:36:59 +0100

    BaseRepo fejléce

--- Kreata.Backend/Repos/Base/BaseRepo.cs
+++ Kreata.Backend/Repos/Base/BaseRepo.cs
@@ -1,6 +1,11 @@
-﻿namespace Kreata.Backend.Repos.Base
+﻿using Kreta.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kreata.Backend.Repos.Base
 {
-    public class BaseRepo
+    public class BaseRepo<TDbContext, TEntity>
+        where TDbContext : DbContext
+        where TEntity : class, IDbEntity<TEntity>, new()
     {
     }
 }
